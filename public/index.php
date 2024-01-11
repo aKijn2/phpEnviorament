9f3f03b812c6ea444cac0860d9ddc18affaa8b41
aKijn2 <mrkinder20041010@gmail.com>
2024-01-11 09:55:15 +0100

    v1

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<html> 
+ <head> 
+   <title>Nuestro primer ejemplo en PHP 7</title> 
+ </head> 
+ 
+  <body>
+ 
+  <?php
+ 
+ echo "¡Hola mundo! <br>";
+ 
+ for($i = 0; $i < 10; $i++)
+  {
+   echo "Línea número: ".$i."<br>";
+  }
+ ?>
+ 
+  </body> 
+  </html>
